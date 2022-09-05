80651cff2d59871e68fdeaa3f8d3453986bc2001
Oksana Konieva <anaska0489@gmail.com>
2022-09-05 14:41:07 +0300

    Update js task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,13 +1,41 @@
-const form = document.querySelector('.feedback-form');
-const formData = {};
+import storageAPI from './storage';
+var throttle = require('lodash.throttle');
 
-function handleInput(event) {
-    const { name, value } = event.target;
-    formData[name] = value;
+let form = document.querySelector('.feedback-form');
+
+initPage()
+form.addEventListener('input', throttle(handleInput, 500));
+form.addEventListener('submit', handleSubmit);
+
+function handleInput (event) {
+    const {name, value} = event.target;
     
-    
-    const formDataJSON = JSON.stringify(formData);
-    console.log(formDataJSON);
-    localStorage.setItem('formKey', formDataJSON);
+    let savedData = storageAPI.load('feedback-form-state');
+    savedData = savedData ? savedData : {};
+    savedData[name] = value;
+    storageAPI.save('feedback-form-state', savedData);
+
+    console.log(handleInput);
 }
-form.addEventListener('input', handleInput);
+
+function initPage () {
+   const savedData = storageAPI.load('feedback-form-state');
+    if (!savedData) {
+        return;
+    }
+    Object.entries(savedData).forEach(([name, value]) => {
+        form.elements[name].value = value;
+    })
+}
+
+function handleSubmit (event) {
+    event.preventDefault();
+    const {
+        elements: {email, message}
+    } = event.currentTarget;
+
+    console.log({email: email.value, message: message.value});
+
+    event.currentTarget.reset();
+    storageAPI.remove('feedback-form-state');
+}
